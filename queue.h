bfec693424988bc0532bdeb3bb03f6a5a1fa151d
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2022-11-15 12:19:17 +0200

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,38 @@
+#include<iostream>
+#pragma once
+using namespace std;
+const int size=100;
+template<class t>
+class Queue{
+private:
+    int front,rear;
+    t arr[size];
+public:
+    Queue():front(-1),rear(-1){}
+    bool is_empty(){return front==-1&&rear==-1;}
+    bool is_full(){return front==0&&rear==size-1;}
+    void insert(t element){
+        if(is_full()){
+            cout<<"queue overflow";
+            return;
+        }
+        if(front==-1)front=0;
+        arr[++rear]=element;
+    }
+    void remove(){
+        if(is_empty()){
+            cout<<"queue underflow";
+            return;
+        }
+        else if (front==rear)front=-1,rear=-1;
+        else front++;
+    }
+    void display(){
+        if(is_empty()){
+            cout<<"stack is empty";
+            return;
+        }
+        for(int i=front;i<=rear;i++)cout<<arr[i]<<" ";
+        cout<<"\n";
+    }
+};
