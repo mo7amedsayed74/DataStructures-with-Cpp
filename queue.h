815b44cf78036b2a13aac7e70c69569d3d2b33cf
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2023-06-24 15:02:13 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -27,6 +27,14 @@ public:
         else if (front==rear)front=-1,rear=-1;
         else front++;
     }
+    t getFront(){
+        assert(!is_empty()); // if queue is empty , this function stop the program and give me the line number of error
+        return arr[front];
+    }
+    t getRear(){
+        assert(!is_empty()); // if queue is empty , this function stop the program and give me the line number of error
+        return arr[rear];
+    }
     void display(){
         if(is_empty()){
             cout<<"stack is empty";
