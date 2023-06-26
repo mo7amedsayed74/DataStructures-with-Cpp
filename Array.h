7707d036dbe3d9e129d2eb14703ad49ed1ae8483
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2023-06-26 18:01:34 +0300

    Update Array.h

--- Array.h
+++ Array.h
@@ -1,43 +1,68 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
 
 template<class t> // allow me to use array for all data type
 class Array {
 private:
-    int size, length;
-    t *items;
+    int maxSize, length;
+    t *arr;
 public:
     Array(int arraySize) {
-        items = new int[arraySize];
-        size = arraySize; // number of items that the array can hold it
+        assert(arraySize > 0); // if condition is false , this function stop the program and give me the line number of error
+        arr = new t[arraySize];
+        maxSize = arraySize; // number of items that the array can hold it
         length = 0; // number of items is already exist
     }
 
-    void fill() {
-        int num;
-        cout << "enter number of items you want to fill : ";
-        cin >> num;
-        if (num > size) {
-            cout << "ERROR! overflow\n";
-            return;
-        }
-        length = num;
-        for (int i = 0; i < num; i++) {
-            cout << "enter item no " << i + 1 << " : ";
-            cin >> items[i];
+    bool isEmpty() {
+        return length == 0;
+    }
+
+    bool isFull() {
+        return length == maxSize;
+    }
+
+    int getSize() {
+        return maxSize; // number of items that the array can hold it
+    }
+
+    int getLength() {
+        return length; // number of items is already exist
+    }
+
+    void printLength() {
+        cout << "the length of your array is : " << length << '\n';
+    }
+
+    void printSize() {
+        cout << "the size of your array is : " << maxSize << '\n';
+    }
+
+    void display() {
+        if (length == 0) {
+            cout << "your list is empty.\n";
+        } else {
+            cout << "array items is : [ ";
+            for (int i = 0; i < length; i++)
+                cout << arr[i] << ' ';
+            cout << "]\n";
         }
     }
 
-    int get_length() { return length; }
+    void fill() {
+        cout << "enter " << maxSize << " item/s to fill your array :\n"; // may be [ 1 item ]
+        for (int i = 0; i < maxSize; i++)
+            cin >> arr[i];
+        length = maxSize;
+    }
 
-    int get_size() { return size; }
-
-    void print_length() { cout << "the length of your array is : " << length << '\n'; }
-
-    void print_size() { cout << "the size of your array is : " << size << '\n'; }
 
     int search(t key) { // key is the value for search
         int index = -1;
         for (int i = 0; i < length; i++) {
-            if (items[i] == key) {
+            if (arr[i] == key) {
                 index = i;
                 break;
             }
@@ -46,71 +71,87 @@ public:
     }
 
     void addItem(t newItem) {
-        if (length == size)
-            cout << "You cant exceed the array size\n";
+        if (length == maxSize) // if(isFull())
+            cout << "sorry! cannot add new item ,your list is full.\n";
         else {
-            items[length] = newItem;
+            arr[length] = newItem;
             length++;
             // we can do this in one line >>>>  items[length++]=newItem;
         }
     }
 
-    void insert(t newItem, int index) { // to add item at any place
-        index--; // user use 1 index but we use 0 index
-        if (index >= size - 1)
+    void insert(t newItem, int index) { // to add item at any place   ,  0 index
+        if (index >= maxSize)
             cout << "Index out of range\n";
-        else if (length == size)
+        else if (length == maxSize)
             cout << "sorry, your list is full\n";
         else if (index > length)
             cout << "sorry, can’t leave empty places\n";
         else {
             for (int i = length; i > index; i--) {
-                items[i] = items[i - 1]; // shift elements
+                arr[i] = arr[i - 1]; // shift elements
             }
-            items[index] = newItem;
+            arr[index] = newItem;
             length++;
-            // we can do this in one line >>>>  items[length++]=newItem;
         }
     }
 
-    void Delete(int index) {
-        if (index >= size)
+    void insertWithoutDuplicate(t newItem) {
+        if (search(newItem) == -1) {
+            addItem(newItem);
+        } else {
+            cout << "This item already exists.\n";
+        }
+    }
+
+    t getItemAt(int index) {
+        if (index < 0 || index >= length) {
+            cout << "out of range...!\n";
+        } else {
+            return arr[index];
+        }
+    }
+
+    void Delete(int index) { // 0 index
+        if (index >= maxSize) {
             cout << "Index out of range\n";
-        else {
-            index--; // user use 1 index but we use 0 index
+        } else if (index >= length) {
+            cout << "There is no item in this place.\n";
+        } else {
             for (int i = index; i < length - 1; i++) {
-                items[i] = items[i + 1];
+                arr[i] = arr[i + 1];
             }
             length--;
         }
     }
 
-    void display() {
-        cout << "array items is : [ ";
-        for (int i = 0; i < length; i++)
-            cout << items[i] << ' ';
-        cout << "]\n";
+    void clear() {
+        length = 0;
     }
 
     void Enlarge(int newSize) { // to increase size of array by make a new array by large size and copy item
-        if (newSize <= size)
+        if (newSize <= maxSize)
             cout << "sorry, New size must be larger\n";
         else {
-            t *old = items; // copy
-            items = new int[newSize]; // edit size
+            t *old = arr; // copy
+            arr = new t[newSize]; // edit size
             for (int i = 0; i < length; i++) {
-                items[i] = old[i]; // restore items
+                arr[i] = old[i]; // restore items
             }
-            size = newSize;
+            maxSize = newSize;
             delete[] old;
         }
     }
 
     void Merge(Array other) { // to merge your array with another
-        int newSize = size + other.get_size();
+        int newSize = maxSize + other.getSize();
         Enlarge(newSize);
-        for (int i = 0; i < other.get_length(); i++) {
-            items[length++] = other.items[i];
+        for (int i = 0; i < other.getLength(); i++) {
+            arr[length++] = other.arr[i];
         }
+    }
+
+    ~Array() {
+        delete[]arr;
     }
 };
