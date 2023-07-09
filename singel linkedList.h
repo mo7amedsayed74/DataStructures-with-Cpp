2ab68802fad2c438c3fedb9c8dc547b3bdd82372
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2023-07-09 23:34:32 +0300

    Update singel linkedList.h

--- singel linkedList.h
+++ singel linkedList.h
@@ -2,16 +2,22 @@
 //Advantages    ( Dynamic size  -  ease of insertion/deletion )
 //disAdvantages (random access isn’t allowed - extra memory space is required for pointer to each element )
 
-struct node {
-    int value;
-    node *next;
-};
+#include <iostream>
+using namespace std;
 
-class linked_list {
+template<class t>
+class linkedList {
+    struct node {
+        t data;
+        node *next;
+    };
+
     node *head;
+    int length;
 public:
-    linked_list() {
+    linkedList() {
         head = NULL;
+        length = 0;
     }
 
     bool isEmpty() {
@@ -22,51 +28,49 @@ public:
          */
     }
 
-    void insert_front(int data) {
+    void insertFront(t element) {
         node *newNode = new node;
-        newNode->value = data;
+        newNode->data = element;
         if (isEmpty()) { // head == NULL
             newNode->next = NULL;
-        }
-        else {
+        } else {
             newNode->next = head;
         }
         head = newNode;
+        length++;
     }
 
-    void insert_back(int data) {
+    void insertBack(t element) {
         node *newNode = new node;
-        newNode->value = data;
+        newNode->data = element;
 
         if (isEmpty()) { // head == NULL
-            newNode->next = NULL;
             head = newNode;
-        }
-       else{
+            newNode->next = NULL;
+        } else {
             node *temp = head;
             while (temp->next != NULL) {
                 temp = temp->next;
             }
             temp->next = newNode;
             newNode->next = NULL;
-       }
+        }
+        length++;
     }
 
-    void insert_pos(int data, int pos) { 
-        if(pos==1){
-            insert_front(data);
+    void insertPos(t element, int pos) { // one index
+        if (pos == 1) {
+            insertFront(element);
             return; // stop function
-        }
-        else if(pos == countElements()+1){
-            insert_back(data);
+        } else if (pos == (length + 1)) {
+            insertBack(element);
             return; // stop function
-        }
-        else if(pos > countElements()+1){
-            cout<<"Sorry ,invalid index\n";
+        } else if (pos < 0 || pos > (length + 1)) {
+            cout << "Sorry ,invalid index\n";
             return; // stop function
         }
         node *newNode = new node;
-        newNode->value = data;
+        newNode->data = element;
         node *cur = head;
         node *pre = new node;
         for (int i = 0; i < pos - 1; i++) {
@@ -77,19 +81,33 @@ public:
         newNode->next = cur;
     }
 
-    void delete_front() {
-        if(isEmpty()){
-            cout<<"ERROR! your list is empty\n";
-            return;
+    int search(t element) {
+        node *pointer = head;
+        int position = 0;
+        while (pointer != NULL) {
+            if (pointer->data == element) {
+                return position; // founded
+            }
+            position++;
+            pointer = pointer->next;
         }
-        node *temp =head;
-        head = head->next;
-        delete temp;
+        return -1; // isn’t found
     }
 
-    void delete_back() {
-        if(isEmpty()){
-            cout<<"ERROR! your list is empty\n";
+    void deleteFront() {
+        if (isEmpty()) {
+            cout << "ERROR! your list is empty\n";
+            return;
+        }
+        node *temp = head;
+        head = head->next;
+        delete temp;
+        length--;
+    }
+
+    void deleteBack() {
+        if (isEmpty()) {
+            cout << "ERROR! your list is empty\n";
             return;
         }
         node *cur = head;
@@ -100,101 +118,170 @@ public:
         }
         pre->next = NULL;
         delete cur;
+        length--;
     }
 
-    void delete_pos(int pos) {
-        if(isEmpty()){
-            cout<<"ERROR! your list is empty\n";
+    void deleteByPos(int pos) {
+        if (isEmpty()) {
+            cout << "ERROR! your list is empty\n";
             return;
         }
-        if(pos==1){
-            delete_front();
+        if (pos == 1) {
+            deleteFront();
             return; // stop function
-        }
-        else if(pos > countElements()){
-            cout<<"Sorry ,invalid index\n";
+        } else if (pos > countElements()) {
+            cout << "Sorry ,invalid index\n";
             return; // stop function
         }
         node *cur = head;
         node *pre = new node;
-        for (int i = 0; i < pos - 1; i++) {
+        for (int i = 1; i < pos; i++) {
             pre = cur;
             cur = cur->next;
         }
         pre->next = cur->next;
         delete cur;
+        length--;
     }
 
-    void sort_LinkedList() {
-        node *p, *q;
-        int temp;
-        for (p = head; p->next != NULL; p = p->next) {
-            for (q = p; q != NULL; q = q->next) {
-                if (p->value > q->value) {
-                    temp = p->value;
-                    p->value = q->value;
-                    q->value = temp;
+    void deleteByElement(t item) {
+        if (isEmpty()) {
+            cout << "your list is empty!\n";
+            return;
+        }
+        int pos = search(item);
+        if (pos == -1) {
+            cout << "the element not found, try again.\n";
+        } else {
+            if (pos == 0)
+                deleteFront();
+            else if (pos == (length - 1))
+                deleteBack();
+            else {
+                node *toRemove = head->next;
+                node *toHold = head;
+                for (int i = 1; i < pos; i++) {
+                    toHold = toRemove;
+                    toRemove = toRemove->next;
+                }
+                toHold->next = toRemove->next;
+                delete toRemove;
+                length--;
+            }
+        }
+    }
+
+    /// you can make this functions in one function
+    void remove() {
+        if (isEmpty()) {
+            cout << "your list is empty!\n";
+            return;
+        }
+        int pos, check;
+        cout
+                << "If you want to remove at position enter (1) \nIf you want to remove a specific element you do not know it’s position enter (2)\n";
+        cin >> check;
+        if (check == 1) {
+            cout << "enter position : ";
+            cin >> pos; // user deal with ( 1 index )
+            pos--;
+        } else if (check == 2) {
+            t item;
+            cout << "enter item please : ";
+            cin >> item;
+            pos = search(item); // return position ( 0 index )
+        } else {
+            cout << "wrong choice, try again.\n";
+            remove(); // start function again
+            return;
+        }
+        // start to check on position
+        if (pos < 0 || pos >= length) {
+            cout << "Out of the range, try again.\n";
+            remove(); // start function again
+            return;
+        }
+        if (pos == 0)
+            deleteFront();
+        else if (pos == length)
+            deleteBack();
+        else {
+            node *pre = head;
+            node *toRemove;
+            for (int i = 1; i < pos; i++) {
+                pre = pre->next;
+            }
+            toRemove = pre->next;
+            pre->next = toRemove->next;
+            delete toRemove;
+            length--;
+        }
+    }
+
+    void sortLinkedList() {
+        node *i, *j;
+        t temp;
+        for (i = head; i->next != NULL; i = i->next) {
+            for (j = i; j != NULL; j = j->next) {
+                if (i->data > j->data) {
+                    temp = i->data;
+                    i->data = j->data;
+                    j->data = temp;
                 }
             }
         }
     }
-    bool isFound(int element){ // to check if element exist in list or not
-        bool exist=false;
-        node *temp =head;
+
+    bool isFound(t element) { // to check if element exist in list or not
+        bool exist = false;
+        node *temp = head;
         while (temp != NULL) {
-            if(temp->value==element){
-                exist=true;
+            if (temp->data == element) {
+                exist = true;
                 break; // to exit from while loop
             }
             temp = temp->next;
         }
-        if(exist)return true;
-        else return false;
+        delete temp;
+        return exist;
+        /*
+         if (exist)return true;
+         else return false;
+         */
     }
-    int countElements(){
-        int cnt=0;
-        node *temp =head;
+
+    int countElements() {
+        int cnt = 0;
+        node *temp = head;
         while (temp != NULL) {
             cnt++;
             temp = temp->next;
         }
+        delete temp;
         return cnt;
-    }
+    } // int countElements(){ return length; }
+
     void display() {
-        node *temp =head;
+        node *temp = head;
         while (temp != NULL) {
-            cout << temp->value << " ";
+            cout << temp->data << " ";
             temp = temp->next;
         }
         cout << '\n';
+        delete temp;
+    }
+
+    void clear() {
+        node *current;
+        while (head != NULL) {
+            current = head;
+            head = head->next;
+            delete current;
+        }
+        length = 0;
+    }
+
+    ~linkedList() {
+        clear();
     }
 };
-
-
-/*
-int main() {
- linked_list x;
-    if(x.isEmpty())cout<<"your list is empty\n";
-
-    x.insert_front(5);
-    x.insert_front(6);
-    x.insert_front(7);
-    x.display();
-    cout<<"you have "<<x.countElements()<<" items in your list\n";
-    x.insert_pos(10, 2);
-    x.display();
-    cout<<"you have "<<x.countElements()<<" items in your list\n";
-    x.delete_back();
-    x.delete_front();
-    x.display();
-    cout<<"you have "<<x.countElements()<<" items in your list\n";
-    x.insert_pos(11, 2);
-    x.display();
-    cout<<"you have "<<x.countElements()<<" items in your list\n";
-    x.delete_pos(2);
-    x.display();
-    cout<<"you have "<<x.countElements()<<" items in your list\n";
-
-    if(x.isFound(10))cout<<"10 is exist\n";
-}
- */
